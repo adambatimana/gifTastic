d2a089165608b131311dbba0ebc08398280ac1a9
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-05-14 11:22:50 -0400

    pseudo code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
 // button class class=btn btn-info
+// url http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+
+// create array of topics
+var topics = [""]
+var gifName = "";
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=dc6zaTOxFJmzC";
+
+//create ajax call for queryURL 
+// create a for loop to add buttons for each item in the array
+
+//append search topic from search button to add to array
+
+//on click event lister for each button to add 10 gifs with ratings
+
+//on click for pause and animate for each gif
